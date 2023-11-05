57948c81064d9b2b1e24c0f915e10513a69268f9
sonflea <dhk_1@naver.com>
2023-11-05 21:44:27 +0900

    test

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -15,7 +15,7 @@ public class StageManager : MonoBehaviour
         stageIdx = 0;
         stageNames = new string[]
         {
-            "s1_1030map", "s1_1030map", "s1_1030map","Boss_map"
+            "s1_1105map", "s1_1105map", "s1_1105map","Boss_map"
         };
     }
 
