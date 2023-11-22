b65a5119adb4c4e62ddbbd056aca3ac7a8de1b61
RumaniX <145528918+RumaniX@users.noreply.github.com>
2023-11-22 14:11:00 +0900

    asdf

---
+++ Assets/Scripts/HpFillAmount.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HpFillAmount : MonoBehaviour
+{
+    public Image P_HP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Bullet"))
+        {
+            GameObject parent = other.GetComponent<Bullet>().parent;
+            if (!parent.tag.Equals(gameObject.tag))
+            {
+                GameManager.Instance.TriggerManager.OnTrigger(PlayTriggerType.EnemyHit);
+                HP.fillAmount = (float)healthPoint / (float)maxHealthPoint;
+                Debug.Log("나 체력 준다");
+                if (healthPoint <= 0)
+                {
+                    GameManager.Instance.TriggerManager.OnTrigger(PlayTriggerType.EnemyDie);
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
