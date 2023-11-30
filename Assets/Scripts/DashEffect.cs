32cca289ab966ee6be5507073bc6401bf8da2869
RumaniX <145528918+RumaniX@users.noreply.github.com>
2023-11-30 16:09:57 +0900

    DashEffect clear

--- Assets/Scripts/DashEffect.cs
+++ Assets/Scripts/DashEffect.cs
@@ -5,17 +5,13 @@ using UnityEngine.UI;
 
 public class DashEffect : MonoBehaviour
 {
-    public Image D_effect;
+    public GameObject D_effect;
     public GameObject Character;
 
-    // Start is called before the first frame update
-    void Start()
+    void start()
     {
-        // 대시 효과의 초기 상태가 올바르게 설정되도록 확인합니다.
-        DashImageOnOff();
+        D_effect.SetActive(false);
     }
-
-    // Update is called once per frame
     void Update()
     {
         // isDash가 true인지 확인하고 대시 효과를 업데이트합니다.
@@ -26,9 +22,11 @@ public class DashEffect : MonoBehaviour
     {
         // Character 스크립트에 isDash가 불리언 속성으로 가정합니다.
         bool isDash = Character.GetComponent<Character>().isDash;
-
-        // isDash 값에 따라 Dash 효과 이미지의 활성화 또는 비활성화를 설정합니다.
-        D_effect.gameObject.SetActive(isDash);
+        if (isDash == true)
+        {
+            D_effect.SetActive(true);
+        }
+        else D_effect.SetActive(false);
     }
 }
 
